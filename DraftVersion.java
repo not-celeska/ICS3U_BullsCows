2aa960b142691e3f5568bc994d20c69bb70b03dc
mbush1 <bushkov.mikhail@gmail.com>
2023-12-24 01:21:29 -0500

    Reformated + began on getCows()

--- DraftVersion.java
+++ DraftVersion.java
@@ -4,16 +4,19 @@ import java.util.Random;
 import java.util.Scanner;
 
 // BULLS AND COWS: BASIC ANALOG VERSION.
-public class DraftVersion {
-
+public class DraftVersion
+{
     public static int digitsInCorrectNumber = 4;
+    public static int correctNumber;
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner userInput = new Scanner(System.in);
 
         // Get the correct number.
-        ArrayList<Integer> correctNumber = generateRandomNumber(digitsInCorrectNumber);
+        correctNumber = generateRandomNumber(digitsInCorrectNumber);
         System.out.println(correctNumber);
+        System.out.println(intToArray(correctNumber));
 
         // User guesses
         int userGuess = getGuess(userInput);
@@ -32,55 +35,65 @@ public class DraftVersion {
     // number of digits? --> 3 4 or 5
 
     // GENERATE RANDOM NUMBER: Just generates a random number with the length of numDigits.
-    public static ArrayList<Integer> generateRandomNumber(int numDigits) {
+    public static int generateRandomNumber(int numDigits)
+    {
         Random numberGenerator = new Random();
-        ArrayList<Integer> randomNumber = new ArrayList<>();
+        String randomNumber = "";
 
         // 1. We will generate [numDigits] random numbers from 0-9.
-        for (int i = 0; i < numDigits; i++) {
-
+        for (int i = 0; i < numDigits; i++)
+        {
             int randomDigit;
 
             // a) check i: 0123 cannot happen <-- conversion from string to integer will cause it to become 123.
-            if (i == 0) {
+            if (i == 0)
+            {
                 randomDigit = numberGenerator.nextInt(1, 10);
             }
-            else {
+            else
+            {
                 randomDigit = numberGenerator.nextInt(0, 10);
             }
 
             // b) Make sure each number in the integer is unique; helps us out later on when checking Bulls and Cows.
-            if (randomNumber.contains(randomDigit) ) {
+            if (randomNumber.contains(String.valueOf(randomDigit)))
+            {
                 numDigits++; // This has the for loop go on one more time.
             }
-            else {
-                randomNumber.add(randomDigit); // Adds it to the random number.
+            else
+            {
+                randomNumber += randomDigit; // Adds it to the random number.
             }
         }
 
-        return randomNumber;
+        return Integer.parseInt(randomNumber);
     }
 
     // GET GUESS: Gets a error-checked guess.
-    public static int getGuess(Scanner userInput) {
-
+    public static int getGuess(Scanner userInput)
+    {
         int userGuess;
 
-        while (true) {
+        while (true)
+        {
 
             System.out.print("Enter your guess [" + digitsInCorrectNumber + "#]: ");
 
-            try { // Try: checks that use inputted an integer.
+            try
+            { // Try: checks that use inputted an integer.
                 userGuess = userInput.nextInt();
 
-                if (String.valueOf(userGuess).length() != digitsInCorrectNumber) { // If: checks that the user inputted the right number of digits.
+                if (String.valueOf(userGuess).length() != digitsInCorrectNumber)
+                { // If: checks that the user inputted the right number of digits.
                     System.out.println("\nThe correct number has " + digitsInCorrectNumber + " digits. Try again.");
                 }
-                else {
+                else
+                {
                     return userGuess;
                 }
             }
-            catch (Exception e) {
+            catch (Exception e)
+            {
                 System.out.println("\nYou did not enter an integer. Try again.");
                 userInput.next();
             }
@@ -88,18 +101,39 @@ public class DraftVersion {
     }
 
     // INT TO ARRAYLIST: Takes an int then coverts it into an array with the integer digits.
-    public static ArrayList<Integer> intToArray(int integer) {
+    public static ArrayList<Integer> intToArray(int integer)
+    {
 
         ArrayList<Integer> intArray = new ArrayList<>();
         String integerString = String.valueOf(integer);
 
-        for (int i = 0; i < integerString.length(); i++) {
+        for (int i = 0; i < integerString.length(); i++)
+        {
             char digitChar = integerString.charAt(i);
             intArray.add(Character.getNumericValue(digitChar));
         }
 
         return intArray;
     }
+
+    // GET COWS: Takes the input and correctNumber, returns bulls (right number, wrong place)
+    public static int getCows(ArrayList<Integer> guessedArray)
+    {
+        ArrayList<Integer> correctArray = intToArray(correctNumber); // TODO optimize this to a class variable.
+
+        int cows = 0;
+
+        // check cows for each digit
+        for (int digitGuessed : guessedArray)
+        {
+            // TODO work on this next...
+        }
+
+        return cows;
+    }
+
+    // GET BULLS: Takes the input and correctNumber, returns bulls (right number and place)
+
 
     // setting: use buttons or a keyboard.
 }
